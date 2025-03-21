dfe48cf7db81ac7411fcc0426aebb7cf8a7ef67e
SudarshanProCoder <sudarshandate21@gmail.com>
2025-03-22 01:56:47 +0530

    add income model, routes, and controller for income management

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const connectDB = require("../backend/config/db");
-const authRoutes = require("../backend/routes/authRoutes");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const incomeRoutes = require("./routes/incomeRoutes");
 const app = express();
 
 ///MIDDLEWARE to handle CORS
@@ -19,6 +20,7 @@ app.use(
 app.use(express.json());
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/income", incomeRoutes);
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
