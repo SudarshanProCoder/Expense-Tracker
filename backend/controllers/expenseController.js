328c7556b9fb1ff96bae67fb983b527be27b313b
SudarshanProCoder <sudarshandate21@gmail.com>
2025-03-22 14:06:58 +0530

    add expense model, routes, and controller; implement add, get, delete, and Excel download functionalities but login remains

---
+++ backend/controllers/expenseController.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+const Income = require("../models/Income");
+const User = require("../models/User");
+const xlsx = require("xlsx");
+const Expense = require("../models/Expense");
+
+//add expense in database
+exports.addExpense = async (req, res) => {};
+
+//get all expense data from database
+exports.getAllExpense = async (req, res) => {};
+
+//delete expense data from database
+exports.deleteExpense = async (req, res) => {};
+
+//download expense data in excel format
+exports.downloadExpenseExcel = async (req, res) => {};
