7e567bfe06b517cc66bf1f7eee81db469ebdc88a
SudarshanProCoder <sudarshandate21@gmail.com>
2025-04-01 01:06:16 +0530

    feat: implement Income component with income data fetching and overview; add IncomeOverview component for transaction display

---
+++ frontend/expense-tracker/src/components/Income/IncomeOverview.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const IncomeOverview = ({ transactions, onaAddIncome }) => {
+  return <div></div>;
+};
+
+export default IncomeOverview;
--- frontend/expense-tracker/src/pages/Dashboard/Income.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Income.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+import IncomeOverview from "../../components/Income/IncomeOverview";
 
 function Income() {
-  return <div>Income</div>;
+  const [incomeData, setIncomeData] = useState([]);
+
+  const [loading, setLoading] = useState(false);
+
+  const [openDeleteAltert, setOpenDeleteAlert] = useState({
+    show: false,
+    data: null,
+  });
+  const [openAddIncomeModel, setOpenAddIncomeModel] = useState(false);
+
+  //fetch all income details
+  const fetchIncomeDetails = async () => {};
+  //Handle Add income
+  const handleAddIncome = async (income) => {};
+  // Delete Income
+  const deleteIncome = async (id) => {};
+  //handle Download income details
+  const handleDownloadIncomeDetails = async () => {};
+  return (
+    <DashboardLayout activeMenu="Income">
+      <div className="my-5 mx-auto">
+        <div className="grid grid-cols-1 gap-6">
+          <div className="">
+            <IncomeOverview
+              transactions={incomeData}
+              onaAddIncome={() => setOpenAddIncomeModel(true)}
+            />
+          </div>
+        </div>
+      </div>
+    </DashboardLayout>
+  );
 }
 
 export default Income;
